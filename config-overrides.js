0adeea98c7e021b7d61b0fa5a696d703eb92ac1d
vladimir.i <vladimir.ivanenko@picsart.com>
2024-10-05 21:27:39 +0400

    config-overrides upd

--- config-overrides.js
+++ config-overrides.js
@@ -2,15 +2,17 @@ const path = require('path');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 
 module.exports = function override(config, env) {
+  if (env !== 'development') return config;
+
   // eslint-disable-next-line no-param-reassign
   config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
 
   // eslint-disable-next-line no-param-reassign
   config.resolve.alias = {
     ...config.resolve.alias,
-    // uncomment the next line for debugging local dependencies that have "react" as peer dependency
-    // (e.g. "react-gestures": "file:../react-gestures")
-    // react: path.resolve('./node_modules/react'),
+    // solves the problem of having "react" as peer dependency
+    // e.g. in case when "react-gestures" installed as a local dependency (via file:/)
+    react: path.resolve('./node_modules/react'),
   };
 
   return config;
